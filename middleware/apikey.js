693e03b0370c841eff43ca2a7ab0f48b45f48cb5
Ricardo Carrasco Cursach <rccursach@gmail.com>
2019-12-02 23:10:52 -0300

    add API_KEY middleware

---
+++ middleware/apikey.js
@@ -0,0 +1,12 @@
+exports.has_api_key = (req, res, next) => {
+
+  const api_key = process.env.API_KEY;
+
+  if (req.header('API-KEY') !== api_key || api_key === undefined) {
+    return res.status(401).send({
+      message: 'Unauthorized'
+    });
+  } else {
+    next();
+  }
+}
